14209a8710043b5a3b755ee679a2a0d630f308aa
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-07-17 01:13:01 +0800

    Update script.js

--- country-search-main/script.js
+++ country-search-main/script.js
@@ -19,6 +19,7 @@ const apiUrl=`https://restcountries.com/v3.1/`;
 
 
 
+
 searchItem.onkeyup = function (e) {if (e. key === 'Enter') {searchBtn.click(); }}
 mql.onchange = (e) => {
     if (e.matches) 
